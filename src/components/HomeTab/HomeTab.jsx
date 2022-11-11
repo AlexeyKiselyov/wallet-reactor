3dbf72c6484919ef4ddcfb906c0633204a6cb890
serheiev <69833095+serheiev@users.noreply.github.com>
2022-11-11 17:04:09 +0200

    add table

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -1,7 +1,202 @@
+import s from './HomeTab.module.css';
+
+const tableData = [
+  {
+    date: '11.12.19',
+    type: '+',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '+',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '-',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '+',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '-',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '-',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '+',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '-',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '+',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '+',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '-',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '+',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '-',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '+',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '+',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '-',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '+',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '-',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '+',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+  {
+    date: '11.12.19',
+    type: '+',
+    category: 13,
+    comment: 'some comment is here, maybe some text',
+    sum: 15,
+    balance: 16,
+  },
+];
+
 export const HomeTab = () => {
   return (
-    <>
-      <div>Home Tab</div>
-    </>
+    <div className={s.tableWrap}>
+      <div className={s.scrollTable}>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Type</th>
+              <th>Category</th>
+              <th>Comment</th>
+              <th>Sum</th>
+              <th>Balance</th>
+            </tr>
+          </thead>
+        </table>
+
+        <div className={s.scrollTableBody}>
+          <table>
+            <tbody>
+              {tableData.map(el => (
+                <tr>
+                  <td>{el.date}</td>
+                  <td>{el.type}</td>
+                  <td>{el.category}</td>
+                  <td>{el.comment}</td>
+                  <td>{el.sum}</td>
+                  <td>{el.balance}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   );
 };
