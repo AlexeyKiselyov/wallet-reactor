2c39d2cf5d5c78708c444f863954d0383b195a5c
Roman Lysenko <romashkaxxxl@gmail.com>
2022-11-13 16:19:08 +0200

    curr&balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,7 +1,20 @@
-export const Balance = () => {
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { transactionsSelectors } from '../../../src/redux/transactions';
+import styles from './Balance.module.css';
+
+const Balance = () => {
+  const balance = useSelector(transactionsSelectors.getBalance);
+  // console.log(balance);
   return (
-    <>
-      <div>Balance</div>
-    </>
+    <div className={styles.wrapper}>
+      <h2 className={styles.title}>Current Balance</h2>
+      <p className={styles.text}>
+        <span className={styles.currency}>&#8372;</span>
+        {balance}
+      </p>
+    </div>
   );
 };
+
+export default Balance;
