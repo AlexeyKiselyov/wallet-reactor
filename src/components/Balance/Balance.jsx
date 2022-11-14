820dd7f2a4559ddd0d3e069e5f8cf8fe0ade81ab
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-11-14 09:17:45 +0200

    some change balance and header

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
-// import { transactionsSelectors } from '../../../src/redux/';
+import { useSelector } from 'react-redux';
+
+import { authSelectors } from 'redux/auth'
 
 import styles from './Balance.module.scss';
 
 const Balance = () => {
-  const balance = 2000;
-  //useSelector(transactionsSelectors.getBalance);
-  // console.log(balance);
+  const balance = useSelector(authSelectors.selectBalance);
+  
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.title}>Current Balance</h2>
       <p className={styles.text}>
         <span className={styles.currency}>&#8372;</span>
-        {balance}
+        {balance}.00
       </p>
     </div>
   );
