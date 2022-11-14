9377ab39816508e31c28933117f4da6caebbda2b
DmytroPopko <popkomu@gmail.com>
2022-11-14 13:49:11 +0200

    clean code

--- src/components/LoginTestUser/LoginTestUser.jsx
+++ src/components/LoginTestUser/LoginTestUser.jsx
@@ -1,8 +1,6 @@
-// import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { TEST_USER } from 'variables';
-// import { useLogInUserMutation, setAuth, setUser, setError } from 'redux/index';
 import Icons from 'images/sprite.svg';
 import style from './LoginTestUser.module.css';
 import { toast } from 'react-toastify';
@@ -10,27 +8,12 @@ import Spinner from 'components/Spinner';
 import { useAuth } from 'hook';
 import { authOperations } from 'redux/auth';
 
-
 export default function LoginTestUser({ showTestMode, onClose }) {
-  // const [loginUser, { data, error, isLoading }] = useLogInUserMutation();
   const dispatch = useDispatch();
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
   const navigate = useNavigate();
   const { isLoading } = useAuth();
 
-  // useEffect(() => {
-  //   try {
-  //     if (data) {
-  //       dispatch(setAuth({ token: data.token }));
-  //       dispatch(setUser(data.user));
-  //     } else if (error) toast.error('Request failed');
-  //   } catch {
-  //     dispatch(setError(500));
-  //   }
-  // }, [data, dispatch, error]);
-
-  const onLogin = () => {    
+  const onLogin = () => {
     dispatch(authOperations.logIn(TEST_USER)).then(response => {
       if (response.payload === 'Request failed with status code 403') {
         toast.error('Oops... Password is incorrect!');
@@ -44,35 +27,33 @@ export default function LoginTestUser({ showTestMode, onClose }) {
         navigate('/home', { replace: true });
       }
     });
-    // resetForm();
   };
 
-  // const resetForm = () => {
-  //   setEmail('');
-  //   setPassword('');
-  // };
-
   return (
-    <div className={style[showTestMode ? 'modalContainer' : 'modalContainerHidden']}>
+    <div
+      className={
+        style[showTestMode ? 'modalContainer' : 'modalContainerHidden']
+      }
+    >
       <div className={style[isLoading ? 'wrapperLoading' : 'wrapper']}>
         <button
-          type='button'
+          type="button"
           className={style.buttonHide}
           onClick={onClose}
-          aria-label='hide test user mode'
+          aria-label="hide test user mode"
         >
-          <svg className={style.icon} width='20' height='20'>
+          <svg className={style.icon} width="20" height="20">
             <use href={`${Icons}#icon-close-cross`}></use>
           </svg>
         </button>
         <p className={style.text}>
           Before creating your personal account you can {'   '}
-          <button className={style.button} type='button' onClick={onLogin}>
+          <button className={style.button} type="button" onClick={onLogin}>
             run demo version
           </button>
         </p>
       </div>
-      {isLoading && <Spinner size={80} color='white' />}
+      {isLoading && <Spinner size={80} color="white" />}
     </div>
   );
 }
