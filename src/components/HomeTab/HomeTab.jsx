053c971cd02175a2d27298350c7016d122766e0a
serheiev <69833095+serheiev@users.noreply.github.com>
2022-11-16 16:37:38 +0200

    added sort

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -89,22 +89,31 @@ export const HomeTab = () => {
               <table>
                 <tbody>
                   {transactions.length ? (
-                    transactionsReverse.map(el => (
-                      <tr key={el.id}>
-                        <td>{el.transactionDate}</td>
-                        <td>{el.type !== 'EXPENSE' ? '+' : '-'}</td>
-                        <td>
-                          {categoriesList.length &&
-                            categoriesList.find(cat => cat.id === el.categoryId)
-                              .name}
-                        </td>
-                        <td>{el.comment}</td>
-                        <td className={el.amount > 0 ? s.positive : s.negative}>
-                          {el.amount}
-                        </td>
-                        <td>{el.balanceAfter}</td>
-                      </tr>
-                    ))
+                    transactionsReverse
+                      .sort(
+                        (a, b) =>
+                          new Date(b.transactionDate) -
+                          new Date(a.transactionDate)
+                      )
+                      .map(el => (
+                        <tr key={el.id}>
+                          <td>{el.transactionDate}</td>
+                          <td>{el.type !== 'EXPENSE' ? '+' : '-'}</td>
+                          <td>
+                            {categoriesList.length &&
+                              categoriesList.find(
+                                cat => cat.id === el.categoryId
+                              ).name}
+                          </td>
+                          <td>{el.comment}</td>
+                          <td
+                            className={el.amount > 0 ? s.positive : s.negative}
+                          >
+                            {el.amount}
+                          </td>
+                          <td>{el.balanceAfter}</td>
+                        </tr>
+                      ))
                   ) : (
                     <tr>
                       <td>No transactions</td>
